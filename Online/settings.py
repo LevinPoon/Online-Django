1a8811111a81f3a3b3292c45dc73a52a67fafbbf
Levin Poon <huangkai8652@hotmail.com>
2022-04-03 13:35:29 +0800

    fix reviews issues

--- Online/settings.py
+++ Online/settings.py
@@ -151,8 +151,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = 'static'
-STATIC_ROOT = BASE_DIR /'static'
+STATIC_ROOT = 'static'
+#STATIC_ROOT = BASE_DIR /'static'
 STATICFILES_DIRS =[
     'Online/static', 
 ]
