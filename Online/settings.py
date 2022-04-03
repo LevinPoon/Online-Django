6eb80cb154ed3a861542f81a4036a49784a74037
Levin Poon <huangkai8652@hotmail.com>
2022-04-03 16:00:35 +0800

    fix reviews issues

--- Online/settings.py
+++ Online/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['Play1.eba-cvmkpqem.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['OnlineDjango-Env.eba-bzaywzi2.us-west-2.elasticbeanstalk.com']
 
 
 
